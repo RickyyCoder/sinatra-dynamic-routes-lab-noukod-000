907581ef1d89012003b545244b9f6f430baf2f28
Ricardo Paul <kingupper0@gmail.com>
2019-05-31 12:51:48 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -15,7 +15,7 @@ class App < Sinatra::Base
   
   get '/say/:word1/:word2/:word3/:word4/:word5' do 
     @sentence = ""
-    @sentence += 
+    @sentence += params[:word1] + params[word2]
   end
   
   get '/say/:number/:phrase' do
